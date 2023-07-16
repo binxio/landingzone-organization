86d2a89436035c1e8ba68720c30e381ae07f3d46
Joris Conijn <joris.conijn@xebia.com>
2023-07-16 16:14:16 +0200

    feat: return environments in DTAP order (#85)
    **Issue #, if available:**
    
    ## Description of changes:
    
    When you retrieve the environments you used to get a random order. With
    this feature you will have the ability to customize the order. By
    default we follow the DTAP order as in Development, Testing, Acceptance
    and Production.
    
    **Checklist**
    
    <!--- Leave unchecked if your change doesn't seem to apply -->
    
    * [x] Update tests
    * [x] Update docs
    * [x] PR title follows [conventional commit
    semantics](https://www.conventionalcommits.org/en/v1.0.0-beta.2/#commit-message-for-a-fix-using-an-optional-issue-number)
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- landingzone_organization/filtering.py
+++ landingzone_organization/filtering.py
@@ -4,9 +4,9 @@ import os
 import re
 from typing import Optional
 
-
 PATTERN_WORKLOAD_NAME = os.environ.get("PATTERN_WORKLOAD_NAME", ".*?-(.*)-.*")
 PATTERN_ENVIRONMENT_NAME = os.environ.get("PATTERN_ENVIRONMENT_NAME", ".*-.*-(.*)")
+ENVIRONMENT_WEIGHT = os.environ.get("ENVIRONMENT_WEIGHT", "dev,test,acc,prod")
 
 
 def match_workload_pattern(name: str) -> bool:
@@ -21,3 +21,10 @@ def resolve_workload_name(name: str) -> Optional[str]:
 def resolve_account_environment(name: str) -> Optional[str]:
     match = re.match(PATTERN_ENVIRONMENT_NAME, name)
     return match.group(1) if match else None
+
+
+def resolve_account_weight(name: str) -> int:
+    order = ENVIRONMENT_WEIGHT.split(",")
+    item = next(filter(lambda x: name.startswith(x), order), None)
+
+    return order.index(item) if item else len(order)
